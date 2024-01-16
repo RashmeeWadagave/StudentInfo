02235bd5e40844eb36159329bb22f41c85d16f41
Rashmee Wadagave <rashmeewadagave105@gmail.com>
2024-01-16 22:31:28 +0530

    support files

---
+++ server/Server/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace Server
+{
+    public class SuperHero
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+    }
+   
+}
